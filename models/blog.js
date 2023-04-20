d86a8642cb865856656d2f771aaedeea615609fe
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-20 23:38:00 +0530

    Updated blog schema and displayed in webpage

--- models/blog.js
+++ models/blog.js
@@ -1,19 +1,27 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-const blogSchema=new Schema({//propeties yu want in your blog
-title:{
-     type:String,
-     required:true
-},
-snippet:{
-     type:String,
-     required:true
-},
-body:{
-type:String,
-required:true
-}
-},{timestamps:true});
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const blogSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
-const Blog=mongoose.model('Blog',blogSchema);
-module.exports=Blog;
+    title: {
+      type: String,
+      required: true,
+    },
+    snippet: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model("Blog", blogSchema);
+module.exports = Blog;
